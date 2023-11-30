73d66afe9db5a21a57bba45205b4ae5079bafcfa
VoVralo <vova.minin2010@gmail.com>
2023-11-30 12:34:04 +0200

    database and settings

---
+++ app/receipts_data.py
@@ -0,0 +1,15 @@
+menu = [
+    {
+        'title': 'Бургер с свининой',
+        'image': 'image(1).png',
+        'cook_time': 60,
+    },    {
+        'title': 'Пицца обыкновенная',
+        'image': 'image(2).png',
+        'cook_time': 90,
+    },    {
+        'title': 'Шашлык в духовке',
+        'image': 'image(3).png',
+        'cook_time': 180,
+    },
+]
